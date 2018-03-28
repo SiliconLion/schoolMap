ef6cccee4ffa9eb120bafd99098f00158d3e674f
Samson Smith <smithsac@hudson.k12.wi.us>
2018-03-28 11:40:20 -0500

    +scan an image to generate nodes + assign all  values

--- imageHandler.js
+++ imageHandler.js
@@ -1,11 +1,10 @@
 'use strict'
 
 
-var scrollX=1;
-var scrollY=1;
 var blueVal = rgbToDecimal([0,0,255]);
-var foundCorners = [];
-var nodeWidth = 20;
+var roomColorVal = rgbToDecimal([?,?,?]);
+var nodeWidth = 13;
+var foundNodes[];
 
 //temporary, for testing purposes
 var height = 800;
@@ -17,24 +16,38 @@ function scanForNodes(){
     for (w = 0; w < width; w++){
 
 
-      if (getPixelColor(scrollX,scrollY) === blueVal
-      && getPixelColor(scrollX+1,scrollY) === blueVal
-      && getPixelColor(scrollX,scrollY + 1) === blueVal
-      && getPixelColor(scrollX - 1,scrollY) !== blueVal
-      && getPixelColor(scrollX,scrollY - 1) !== blueVal)  {
+      if (getPixelColor(w,h) === blueVal
+      && getPixelColor(w+1,h) === blueVal
+      && getPixelColor(w,h+1) === blueVal
+      && getPixelColor(w-1,h) !== blueVal
+      && getPixelColor(w,h-1) !== blueVal)  {
 
-        i = i + nodeWidth - 1;
+        w = w + nodeWidth - 1;  //Skips the next few values which we know will be blue because they are part of the node we just found
         console.log("I've struck gold! ...blue gold.");
+        //Adds node coordinates to an array for future use in drawing connections.
+        foundNodes.push(w+(nodeWidth/2)-1); //Saves the x and y value of the nodes center ((w or h)+(nodeWidth/2)-1)
+        foundNodes.push(h+(nodeWidth/2)-1); //rather than the x and y of the top left corner (w or h)
+        iFoundANode(w+(nodeWidth/2)-1,h+(nodeWidth/2)-1); //Generates a node at the proper x and y values and inputs it's color ID
       }
     }
   }
 }
 
 function getPixelColor(x,y){
-  pixelColor = rgbToDecimal(getColorAtPixleNTX(scrollX,scrollY));
-  return pixelColor;
+  return rgbToDecimal(getColorAtPixleNTX(scrollX,scrollY));
 }
 
 function getColorAtPixleNTX(x,y){
-  ntx.getImageData(x, y, 1, 1).data
+  return ntx.getImageData(x,y, 1, 1).data;
+}
+
+function whichLocation(x,y){
+  if (getPixelColor(x+1,y)===roomColorVal)
+    return "room";
+  else
+    return "hallway";
+}
+
+function iFoundANode(x,y){
+  makeNodeFromCoords(x,y,7,getPixelColor(x, y),whichLocation(x,y));
 }
